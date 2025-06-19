5bdd33883468b13eef3c6d32ca88e38b83c15f6b
Salvador Lopez <jslb_cafcb10@hotmail.com>
2025-06-18 23:26:11 -0600

    Omite ordenamiento de alimentos para que el orden se aplique directo en el cuestionario de la app

--- controllers/nuevos_alimentos.ts
+++ controllers/nuevos_alimentos.ts
@@ -4,10 +4,11 @@ import NuevoAlimento from "../models/nuevos_alimentos";
 export const getAlimentos = async (req: Request, res: Response) => {
   const alimentos = await NuevoAlimento.findAll({
      //attributes:["nombre","grupo"],
-    
+      /*
       order: [
         ['nombre', 'ASC'] // Ordenar alfabéticamente por nombre en orden ascendente
       ]
+        */
     });
   res.json({ alimentos });
 };
