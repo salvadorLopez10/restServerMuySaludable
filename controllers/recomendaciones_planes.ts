0e67c9ecff3b0af06b91f4023bbce1cae5ed721d
Salvador Lopez <jslb_cafcb10@hotmail.com>
2025-07-07 23:38:18 -0600

    Creación de endpoint para generar m{ultiples registros de recomendaciones y ajuste para obtener únicamente registros de recomendaciones visibles

--- controllers/recomendaciones_planes.ts
+++ controllers/recomendaciones_planes.ts
@@ -2,7 +2,19 @@ import { Request, Response } from "express";
 import Recomendaciones from "../models/recomendaciones_planes";
 
 export const getRecomendaciones = async (req: Request, res: Response) => {
-  const recomendaciones = await Recomendaciones.findAll();
+  const recomendaciones = await Recomendaciones.findAll({
+    order: [['orden', 'ASC']],
+    where: {
+      visible: true
+    }
+  });
+  
+  if (recomendaciones.length === 0) {
+    return res.status(404).json({
+      msg: "No hay recomendaciones disponibles",
+    });
+  }
+
   res.json({ recomendaciones });
 };
 
@@ -38,6 +50,41 @@ export const postRecomendacion = async (req: Request, res: Response) => {
     });
 
     res.json(rutinaCreada);
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      msg: "Error: Contacte al administrador",
+    });
+  }
+};
+
+
+export const postRecomendaciones = async (req: Request, res: Response) => {
+  const { body } = req;
+
+  try {
+    console.log("BODY Recomendaciones");
+    console.log(body);
+
+    // Verificar si el body es un array
+    if (!Array.isArray(body)) {
+      return res.status(400).json({
+        msg: "El cuerpo de la solicitud debe ser un arreglo de recomendaciones",
+      });
+    }
+
+    // Crear todas las recomendaciones usando bulkCreate
+    const recomendacionesCreadas = await Recomendaciones.bulkCreate(body.map(item => ({
+      objetivo: item.objetivo,
+      titulo: item.titulo,
+      descripcion: item.descripcion,
+      image_url: item.image_url,
+      orden: item.orden,
+      visible: item.visible,
+    })));
+
+    res.json(recomendacionesCreadas);
 
   } catch (error) {
     console.log(error);
--- routes/recomendaciones_planes.ts
+++ routes/recomendaciones_planes.ts
@@ -3,6 +3,7 @@ import {
     getRecomendaciones,
     getRecomendacion,
     postRecomendacion,
+    postRecomendaciones,
 } from "../controllers/recomendaciones_planes";
 
 const router = Router();
@@ -10,5 +11,6 @@ const router = Router();
 router.get("/", getRecomendaciones);
 router.get("/:id", getRecomendacion);
 router.post("/", postRecomendacion);
+router.post("/bulk", postRecomendaciones);
 
 export default router;
