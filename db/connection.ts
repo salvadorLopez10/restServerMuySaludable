937d040c54763310b41ab0601655b30e7d6c9b5a
José Salvador Lopez Balleza <jslb_cafcb10@hotmail.com>
2025-08-26 23:31:47 -0600

    Ajustes para despliegue ambiente QA

--- db/connection.ts
+++ db/connection.ts
@@ -3,8 +3,27 @@ import mysql2 from 'mysql2';
 import dotenv from "dotenv";
 import path from "path";
 
-const envFile = process.env.NODE_ENV === "production" ? ".env.production" : ".env.development";
-dotenv.config({ path: path.resolve(__dirname, `../../${envFile}`) });
+// const envFile = process.env.NODE_ENV === "production" ? ".env.production" : ".env.development";
+// dotenv.config({ path: path.resolve(__dirname, `../../${envFile}`) });
+// Determinar el archivo de entorno basado en NODE_ENV
+const getEnvFile = () => {
+  switch (process.env.NODE_ENV) {
+    case 'production':
+      return '.env.production';
+    case 'qa':
+      return '.env.qa';
+    default:
+      return '.env.development';
+  }
+};
+
+const envFile = getEnvFile();
+const envPath = path.resolve(__dirname, `../../${envFile}`);
+
+// Cargar variables de entorno
+dotenv.config({ path: envPath });
+
+console.log(`AMBIENTE Y USUARIO: ${process.env.NODE_ENV} ,${process.env.DB_NAME}, ${process.env.DB_USER}` )
 
 const db = new Sequelize(
   process.env.DB_NAME || "",
